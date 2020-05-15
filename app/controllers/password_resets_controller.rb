df2fbd82f5df0559df50fc64455e8dcaf0c18068
fuwa <fuwafuwa41.glr@gmail.com>
2020-05-15 00:04:27 +0000

    Add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -28,6 +28,7 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)          # (4) への対応
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
@@ -42,46 +43,7 @@ class PasswordResetsController < ApplicationController
     params.require(:user).permit(:password, :password_confirmation)
   end
 
-  def get_user
-      @user = User.find_by(email: params[:email])
-  end
-
-  # 正しいユーザーかどうか確認する
-  def valid_user
-    unless (@user && @user.activated? &&
-            @user.authenticated?(:reset, params[:id]))
-      redirect_to root_url
-    end
-  end
-
- #トークンが期限切れかどうか確認する
-  def check_expiration
-     if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired."
-      redirect_to new_password_reset_url
-     end
-  end
-
-  def update
-    if params[:user][:password].empty?                  # (3) への対応
-      @user.errors.add(:password, :blank)
-      render 'edit'
-    elsif @user.update_attributes(user_params)          # (4) への対応
-      log_in @user
-      flash[:success] = "Password has been reset."
-      redirect_to @user
-    else
-      render 'edit'                                     # (2) への対応
-    end
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:password, :password_confirmation)
-    end
-
-    # beforeフィルタ
+  # beforeフィルタ
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -102,5 +64,4 @@ class PasswordResetsController < ApplicationController
         redirect_to new_password_reset_url
       end
     end
-
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,10 +16,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
-    assert_select 'div.alert'  
-    assert_select 'form[action="/signup"]' 
+    assert_select 'div.field_with_errors'
   end
-  
+
   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
